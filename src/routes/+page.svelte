65be7718717822ea59fdcc1470f6e00e1cf4772f
Zac Jones <zacjones93@gmail.com>
2024-10-24 16:39:13 -0600

    Initial commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,21 @@
+<h1 class="text-2xl font-bold">Welcome to SvelteKit</h1>
+<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+
+{count}
+{countTimesTwo}
+<button class="bg-blue-500 text-white px-4 py-2 rounded-md"
+onclick={() => (count += 1)}>Increment</button>
+<button class="bg-red-500 text-white px-4 py-2 rounded-md"
+onclick={() => (count -= 1)}>Decrement</button>
+
+
+<script>
+  let count = $state(0);
+
+  let countTimesTwo = $derived(count * 2)
+
+  $effect(() => {
+    console.log(count)
+  })
+
+</script>
